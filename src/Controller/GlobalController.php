128334ea204f4a76b9456b2f3b46f301f82fc6d0
sylpi <syl.pillet@hotmail.fr>
2020-09-12 13:01:25 +0200

    first design on home

---
+++ src/Controller/GlobalController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GlobalController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('global/index.html.twig', [
+            'controller_name' => 'GlobalController',
+        ]);
+    }
+}
