1cf4a2e78671c95b7887c1fb1199426b137a1cea
sylpi <syl.pillet@hotmail.fr>
2020-09-14 16:04:43 +0200

    hide nav onclick, responsive on about

--- public/script/script.js
+++ public/script/script.js
@@ -14,3 +14,12 @@ function backToTop() {
        }
  }
 
+// hide menu onclick
+ $(function(){ 
+  let navMain = $(".navbar-collapse"); // avoid dependency on #id
+  // "a:not([data-toggle])" - to avoid issues caused
+  // when you have dropdown inside navbar
+  navMain.on("click", "a:not([data-toggle])", null, function () {
+      navMain.collapse('hide');
+  });
+});
