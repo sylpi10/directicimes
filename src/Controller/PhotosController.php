19f3befd751cca4857600c73c28fcb7967e049e7
sylpi <syl.pillet@hotmail.fr>
2020-10-15 16:52:50 +0200

    update admin templates

--- src/Controller/PhotosController.php
+++ src/Controller/PhotosController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AlbumRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,11 +12,12 @@ class PhotosController extends AbstractController
     /**
      * @Route("/photos", name="photos")
      */
-    public function pictures(AlbumRepository $albumRepository)
+    public function pictures(AlbumRepository $albumRepository, Request $request)
     {
+        // $request->getSession()->set('_locale', $locale);
         $albums = $albumRepository->findAll();
         return $this->render('photos/photos.html.twig', [
-            'albums' => $albums
+            'albums' => $albums,
         ]);
     }
 }
